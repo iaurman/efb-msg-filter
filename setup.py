256eee2056047b411f5963d4be8486c5dfd4be6c
1ndeed <46162162+1ndeed@users.noreply.github.com>
2021-09-19 00:53:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='efb-msg-filter',
     version='0.0.1',
-    description='Filter Only Shoudao Message Only for EQS.'
+    description='Filter Only Shoudao Message Only for EQS.',
     author='Riley Soong',
     auther_email='aurman@qq.com',
     url='https://github.com/1ndeed/efb-msg-filter',
