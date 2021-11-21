5ddd9da9d0c0d9db8785b515412433c1e890f4ce
1ndeed <46162162+1ndeed@users.noreply.github.com>
2021-11-21 16:06:38 +0800

    Update __init__.py

--- efb_msg_filter/__init__.py
+++ efb_msg_filter/__init__.py
@@ -13,6 +13,12 @@ class FilterMiddleware(Middleware):
 
     def process_message(self, message: Message) -> Optional[Message]:
         if message.chat.uid.split('_',1)[0] == 'group':
-            if message.text == '收到':
+            if   message.text == '收到':
+                return None
+            elif message.uid[0:16] == '__group_notice__' and ') joined the group(' in message.text:
+                return None
+            elif message.uid[0:16] == '__group_notice__' and ') quited the group(' in message.text:
+                return None
+            elif message.uid[0:16] == '__group_notice__' and ') uploaded a file to group(' in message.text:
                 return None
         return message
