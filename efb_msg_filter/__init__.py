8b7dd486d8e706317e5dc562b5b00c7adeb1f3d1
1ndeed <46162162+1ndeed@users.noreply.github.com>
2021-12-23 23:25:40 +0800

    Add Filter Function For EWS

--- efb_msg_filter/__init__.py
+++ efb_msg_filter/__init__.py
@@ -6,19 +6,24 @@ from ehforwarderbot import Middleware, Message
 class FilterMiddleware(Middleware):
     middleware_id: str = "rileysoong.msg_filter"
     middleware_name: str = "EFB MsgFilter Middleware"
-    __version__: str = "0.0.2"
+    __version__: str = "0.0.3"
 
     def __init__(self, instance_id: str = None):
         super().__init__(instance_id)
 
     def process_message(self, message: Message) -> Optional[Message]:
-        if message.chat.uid.split('_',1)[0] == 'group':
-            if   message.text == '收到':
-                return None
-            elif message.uid[0:16] == '__group_notice__' and ') joined the group(' in message.text:
-                return None
-            elif message.uid[0:16] == '__group_notice__' and ') quited the group(' in message.text:
-                return None
-            elif message.uid[0:16] == '__group_notice__' and ') uploaded a file to group(' in message.text:
-                return None
+        if   message.chat.module_name == 'QQ Slave':
+            if str(message.chat).split(':')[0][1:] == 'GroupChat':
+                if   message.text == '收到':
+                    return None
+                elif message.uid[0:16] == '__group_notice__' and ') joined the group(' in message.text:
+                    return None
+                elif message.uid[0:16] == '__group_notice__' and ') quited the group(' in message.text:
+                    return None
+                elif message.uid[0:16] == '__group_notice__' and ') uploaded a file to group(' in message.text:
+                    return None
+        elif message.chat.module_name == 'WeChat Slave':
+            if str(message.chat).split(':')[0][1:] == 'GroupChat':
+                if   message.text == '收到':
+                    return None
         return message
