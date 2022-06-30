e90328b30bcb3ddb1aad37abcdc4dc07b6f14588
1ndeed <46162162+1ndeed@users.noreply.github.com>
2022-06-30 17:00:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='efb-msg-filter',
     packages=find_packages(),
     version='0.1.0',
-    description='Advanced Features For EQS, ECWS, EWS.',
+    description='Advanced Features For EQS, EWCS, EWS.',
     author='Riley Soong',
     auther_email='aurman@qq.com',
     url='https://github.com/1ndeed/efb-msg-filter',
